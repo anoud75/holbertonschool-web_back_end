548cad2f19efcdfcd465c5d0be6918e5899a335c
“anoud75” <anoudsaud75@gmail.com>
2026-02-21 19:51:01 +0000

    Add python_variable_annotations task 0-add.py

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Module for basic type annotations with add function."""
+
+
+def add(a: float, b: float) -> float:
+    """Add two float numbers and return their sum."""
+    return a + b
