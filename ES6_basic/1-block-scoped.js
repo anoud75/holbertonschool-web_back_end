3b4d97b6b91fcd2126a4c200106ec4062fdb2290
anoud75 <alanoudsaud75@gmail.com>
2026-04-07 15:47:31 +0000

    Fix task 1 - block scope

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -3,8 +3,8 @@ export default function taskBlock(trueOrFalse) {
   let task2 = true;
 
   if (trueOrFalse) {
-    let task = true; // eslint-disable-line no-unused-vars
-    let task2 = false; // eslint-disable-line no-unused-vars
+    task = false;
+    task2 = true;
   }
 
   return [task, task2];
